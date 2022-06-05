94579cf490acea712af194b0cb30300b7b1bec69
Revanth <31564415+revanthpendyala@users.noreply.github.com>
2022-06-05 12:13:51 -0400

    update2

--- subdom-run.sh
+++ subdom-run.sh
@@ -10,9 +10,32 @@ if [ ! -d  "$url/recon" ];then
 	mkdir $url/recon
 fi 
 
+if [ ! -d "$url/recon/potential_takeovers" ];then
+	mkdir $url/recon/potential_takeovers
+fi
+
+#    if [ ! -d '$url/recon/eyewitness' ];then
+#        mkdir $url/recon/eyewitness
+#    fi
+
+if [ ! -d "$url/recon/scans" ];then
+	mkdir $url/recon/scans
+fi
+
 echo " [+] Harvesting subdomains with assetfinder ..." 
 assetfinder $url >> $url/recon/assets.txt
 cat $url/recon/assets.txt | grep $1 >> $url/recon/final-subdomains.txt
 rm $url/recon/assets.txt
  
+echo "[+] Checking for possible subdomain takeover..."
+if [ ! -f "$url/recon/potential_takeovers/potential_takeovers.txt" ];then
+	touch $url/recon/potential_takeovers/potential_takeovers.txt
+fi
+subjack -w $url/recon/final.txt -t 100 -timeout 30 -ssl -c ~/go/src/github.com/haccer/subjack/fingerprints.json -v 3 -o $url/recon/potential_takeovers/potential_takeovers.txt
 
+echo "[+] Scanning for open ports..."
+nmap -iL $url/recon/httprobe/alive.txt -T4 -oA $url/recon/scans/scanned.txt
+
+
+#echo "[+] Running eyewitness against all compiled domains..."
+#python3 EyeWitness/EyeWitness.py --web -f $url/recon/httprobe/alive.txt -d $url/recon/eyewitness --resolve
